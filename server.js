5c771e79a2366264a3fcd7d9c7cc53b4bf8973d3
NguyenQuocNhu <nquocnhu95it@gmail.com>
2024-02-20 16:04:26 +0700

    redering template engine

--- server.js
+++ server.js
@@ -2,9 +2,18 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+//CONFIGURE TEMPLATE ENGINE
+app.set('views', './src/views')
+app.set('view engine', 'ejs')
+
+//DECLEARING ROUTES
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+app.get('/nhubackend', (req, res) => {
+    //res.send('Hello new path')
+    res.render('sample.ejs')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
