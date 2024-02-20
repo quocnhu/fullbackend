61799422d91571550add5d3bd637ad63abd195bb
NguyenQuocNhu <nquocnhu95it@gmail.com>
2024-02-20 16:37:14 +0700

    ENV

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path'); //in case you template location does not work (is available)
+const app = express();
+const port = 3000;
 
 //CONFIGURE TEMPLATE ENGINE
-app.set('views', './src/views')
+// app.set('views', './src/views')//this place used to teach where is rendering engine template located
+app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs')
 
 //DECLEARING ROUTES
@@ -12,7 +14,7 @@ app.get('/', (req, res) => {
 })
 app.get('/nhubackend', (req, res) => {
     //res.send('Hello new path')
-    res.render('sample.ejs')
+    res.render('sample.ejs') // dynamic rendering 
   })
 
 app.listen(port, () => {
