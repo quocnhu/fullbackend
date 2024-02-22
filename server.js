7cf5cb44530d72faccaa29b91f2f4fbd73a7d748
NguyenQuocNhu <nquocnhu95it@gmail.com>
2024-02-22 13:37:17 +0700

    env error: root

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path'); //in case you template location does not work (is available)
 const app = express();
-const port = 3000;
+//SET UP ENV
+require('dotenv').config();
+const port = process.env.PORT
+
 
 //CONFIGURE TEMPLATE ENGINE
-// app.set('views', './src/views')//this place used to teach where is rendering engine template located
+  // app.set('views', './src/views')//this place used to teach where is rendering engine template located
 app.set('views', path.join(__dirname, './src/views'));
 app.set('view engine', 'ejs')
 
